858edc0516227a334e41564fd2ef2a575a525eb3
Fravic <fravicf@gmail.com>
2014-10-27 22:42:08 -0700

    Add SelectedPieceControls

---
+++ SugarStomp/Assets/Script/Controls/SelectedPieceControls.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class SelectedPieceControls : MonoBehaviour {
+  public static bool CheckUsePower() {
+    GUI.Label(new Rect(0, 0, 250, 30), "Jump Proof");
+    return false;
+  }
+}
--- SugarStomp/Assets/Script/GameManager.cs
+++ SugarStomp/Assets/Script/GameManager.cs
@@ -31,6 +31,10 @@ public class GameManager : MonoBehaviour {
     NextTurn();
   }
 
+  void OnGUI() {
+    SelectedPieceControls.CheckUsePower();
+  }
+
   void CreateBoard() {
     GameObject board = GameObject.FindGameObjectWithTag("Board");
 
